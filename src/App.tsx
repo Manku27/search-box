f6f12748e74a02c771f43e14a45913fd73b25bed
Mayank Jhunjhunwala <v-mayankj@microsoft.com>
2024-05-17 21:21:00 +0530

    add search list page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { SearchBar } from "./search/SearchBar";
+import { NoResults } from "./list/NoResults";
+import { Result } from "./types";
+import { List } from "./list/List";
+import { getFilteredResult } from "./search/getFilteredResult";
 
 function App() {
+  const [results, setResults] = useState<null | Result[]>(null);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    fetch(
+      "https://fe-take-home-assignment.s3.us-east-2.amazonaws.com/Data.json"
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        setResults(json);
+      });
+  }, []);
+
+  const filteredResult = results ? getFilteredResult(results, query) : [];
+
   return (
     <>
       <h1>Search Box</h1>
+      <div>
+        <SearchBar setQuery={setQuery} query={query} />
+        {filteredResult && filteredResult.length == 0 ? <NoResults /> : null}
+        {filteredResult && filteredResult.length > 0 ? (
+          <List results={filteredResult} />
+        ) : null}
+      </div>
     </>
   );
 }
